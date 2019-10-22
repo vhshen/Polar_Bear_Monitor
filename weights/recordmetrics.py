09c4c23dc9d6bb273c589d7c8454c0ff3d55c4e8
vivi-shen <vivianhshen@gmail.com>
2019-10-21 18:18:00 -0700

    new results

--- weights/recordmetrics.py
+++ weights/recordmetrics.py
@@ -1,12 +1,22 @@
 import os
 
-f = open("results.txt", "a")
-for weightfile in os.listdir("test_weights"):
-    thresh = [0.01, 0.25, 0.5, 0.6, 0.75]
-    for t in thresh:
-        f.write("************************************************************")
-        f.write("********** weight file: " + weightfile + "**************")
-        f.write("************** thresh = " + str(t) + " ********************")
-        f.write("************************************************************")
-        command = "./darknet detector map data/pb.data cfg/yolov3-pb.cfg test_weights/" + weightfile + " -thresh " + str(t) + " < data/test.txt >> results.txt"
-        os.system(command)
+wfolder = "../weights/"
+
+tiny = False
+o = open("order.txt", "w")
+for weightfile in os.listdir(wfolder):
+    if "weights" not in weightfile:
+        continue
+    print("Weightfile: " + weightfile)
+
+    animal, rest = weightfile.split("_")
+    if "tiny" in animal:
+        tiny = True
+        animal = animal.split("-")[1]
+    
+    o.write("weight file: " + weightfile + "\n")
+    if tiny:
+        command = "./darknet detector map data/" + animal + ".data cfg/yolov3-tiny-custom.cfg " + wfolder + weightfile + " < data/"+ animal + "test.txt >> results.txt"
+    else:    
+        command = "./darknet detector map data/" + animal + ".data cfg/yolov3-custom.cfg " + wfolder + weightfile + " < data/"+ animal + "test.txt >> results.txt"
+    os.system(command)
